2c06be6d9312ffc1ca80b53513c5a199a6f7e9ff
nemgu <xiufszh@naver.com>
2024-05-18 18:36:50 +0900

    #3.1

--- src/server.js
+++ src/server.js
@@ -8,3 +8,5 @@ const handleListening = () => console.log(`✅ Server listenting on port http://
 
 app.listen(PORT, handleListening) // 바닐라 JS의 click event랑 비슷하다.
 
+// GET은 http method이다.
+// GET은 페이지를 가져오란 뜻이다.
